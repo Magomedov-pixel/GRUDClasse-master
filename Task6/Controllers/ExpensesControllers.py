ec6f31612757690ddfbb5b7998dd743464798dc2
ali <am3434545@gmail.com>
2025-10-31 18:27:32 +0300

    сумма по категории,расходы за период понять работу.

---
+++ Task6/Controllers/ExpensesControllers.py
@@ -0,0 +1,24 @@
+from Task6.Models.Expenses import Expenses
+class ExpensesController:
+    '''
+    добавить расход,
+    сумма по категории,
+    расходы за период
+    '''
+    obj = Expenses()
+
+    @classmethod
+    def get(cls):
+        return cls.obj.expense
+
+    @classmethod
+    def set(cls, amount, category, date, description,):
+        cls.obj.expense = {"amount": amount, "category": category, "date": date, "description": description}
+    @classmethod
+    def update(cls,id,new_amound):
+        for dict in cls.get():
+            if dict['id'] == id:
+                dict['amount'] = new_amound
+if __name__ == '__main__':
+    ExpensesController.set(1,"<ddd>","<ddd>","<ddd>")
+    print(ExpensesController.get())
---
+++ Task6/Models/Expenses.py
@@ -0,0 +1,20 @@
+class Expenses:
+    def __init__(self,):
+        self.__list_expenses = [
+            {"id": 1, "amount": 1500, "category": "Еда", "date": "2024-01-20","description": "Продукты"}
+        ]
+        self.id = 2
+    @property
+    def expense(self):
+        return self.__list_expenses
+    @expense.setter
+    def expense(self, dict):
+        dict['id'] = self.id
+        self.expense.append(dict)
+        self.id += 1
+
+if __name__ == '__main__':
+    exp = Expenses()
+    print(exp.expense)
+    exp.expense = {"amount": 1500, "category": "Еда", "date": "2024-01-20","description": "Продукты"}
+    print(exp.expense)
