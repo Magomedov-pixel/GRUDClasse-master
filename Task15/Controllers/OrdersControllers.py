2afc128ae39b3c0ff87d26202b637f6cc2199835
ali <am3434545@gmail.com>
2025-11-02 14:55:37 +0300

    выполнить tk

--- Task15/Controllers/OrdersControllers.py
+++ Task15/Controllers/OrdersControllers.py
@@ -1,6 +1,26 @@
 from Task15.Models.Orders import Order
 class OrderController:
+    '''изменить статус, заказы клиента, отменить заказ'''
     obj = Order()
     @classmethod
     def get(cls):
         return cls.obj.orders
+    @classmethod
+    def update_status(cls,id,new_status):
+        for dict in cls.get():
+            if dict['id'] == id:
+                dict['status'] = new_status
+    @classmethod
+    def customers(cls, customer):
+        result = f"нет {customer}"
+        for dict in cls.get():
+            if dict['customer'] == customer:
+                result = f"есть {customer}"
+        return result
+    @classmethod
+    def delete(cls, id):
+        for dict in cls.get():
+            if dict['id'] == id:
+                cls.get().remove(dict)
+if __name__ == '__main__':
+    print(OrderController.get())
