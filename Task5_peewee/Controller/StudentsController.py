08d971c0ff2b891269344fdc57b18e0551bf08bb
java <magomedoviamk@gmail.com>
2025-11-10 10:17:53 +0500

    поднял базу данных добавил методы добавление,обновить рейтинг,вывести список запсей по определёномму значению db

--- Task5_peewee/Controller/StudentsController.py
+++ Task5_peewee/Controller/StudentsController.py
@@ -17,12 +17,12 @@ class StudentsController:
             grade = grade,
         )
     @classmethod
-    def rating_update(cls,id,grade):
+    def grade_update(cls,id,grade):
         #изменить оценку
         Students.update({Students.grade:grade}).where(Students.id==id).execute()
 
     @classmethod
-    def get_title(cls,name):
+    def get_name(cls,name):
         #найти по имени
         return Students.select().where(Students.name==name)
     @classmethod
@@ -31,3 +31,8 @@ class StudentsController:
         Students.delete_by_id(id)
 
 if __name__ == '__main__':
+    StudentsController.add(
+        'Игорь',
+        19,
+        "A",
+    )
