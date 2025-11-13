7f0889256d1121c3aea1cbe4620239a427bfa044
java <magomedoviamk@gmail.com>
2025-11-13 11:29:39 +0500

    доделать EmployeesController выполнил методы

--- Task8_peewee/Controller/EmployeesController.py
+++ Task8_peewee/Controller/EmployeesController.py
@@ -18,4 +18,16 @@ class EmploeesController:
         )
     @classmethod
     #повысить зарплату,
-
+    def salary_update(cls,id,salary):
+        Employees.update({Employees.salary:salary}).where(Employees.id == id).execute()
+    @classmethod
+    def get_departament(cls,department):
+        #сотрудники отдела
+        return Employees.select().where(Employees.department==department)
+    @classmethod
+    #уволить
+    def delite(cls,id):
+        Employees.delete_by_id(id)
+if __name__ == '__main__':
+    (EmploeesController
+     .add("viktor", 'fklv', 10000, "flfj"))
